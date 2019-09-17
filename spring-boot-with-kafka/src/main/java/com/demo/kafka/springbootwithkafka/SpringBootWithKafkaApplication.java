12363964b962e24a773ead609b3cb95fb3d7e12c
Douglas Fonseca <vte_dfonseca@uolinc.com>
2019-09-17 15:35:28 -0300

    Projeto de spring boot escrevendo mensagem simples em kafka~

---
+++ spring-boot-with-kafka/src/main/java/com/demo/kafka/springbootwithkafka/SpringBootWithKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.demo.kafka.springbootwithkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithKafkaApplication.class, args);
+	}
+
+}
---
+++ spring-boot-with-kafka/src/main/java/com/demo/kafka/springbootwithkafka/models/User.java
@@ -0,0 +1,10 @@
+public class User {
+
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
